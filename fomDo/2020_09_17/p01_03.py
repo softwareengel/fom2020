84bbe461d8de1146279485466f4d1ac5ecb08bbd
Pro Engels <softwareengel@users.noreply.github.com>
2020-09-17 20:51:08 +0200

    Update p01_03.py

--- fomDo/2020_09_17/p01_03.py
+++ fomDo/2020_09_17/p01_03.py
@@ -1,1 +1,20 @@
 
+# Initialisierung
+
+geheimzahl = 4712
+eingabe = 0
+zaehler = 0
+
+# Schleifenkopf
+while eingabe != geheimzahl:
+    # Schleifenkörper
+    eingabe = int(input("Ganze Zahl eingeben: "))
+        
+    if (eingabe < geheimzahl):
+        print("Zahl zu klein")
+    if (eingabe > geheimzahl):
+        print("Zahl zu groß")
+     
+    zaehler = zaehler + 1
+
+print("Richtig! Sie haben ", zaehler , "Versuche benötigt.")
